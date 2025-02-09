3d188b4d7923286cc0d2a9c68ea7c370f84313d9
jamie <j.parkinson@wellcome.org>
2025-02-09 16:27:24 +0000

    No idea what I'm doing here

--- webapp/src/components/OutcomeCard.tsx
+++ webapp/src/components/OutcomeCard.tsx
@@ -1,6 +1,17 @@
+import dayjs from "dayjs";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
-type Outcome = any;
+type Outcome = {
+  outcome_title: string;
+  outcome_url: string;
+  last_updated: string;
+  derived_query: {
+    union_name: string;
+    employer_name: string;
+  };
+  documents: object[];
+};
 
 type Props = {
   outcome: Outcome;
@@ -11,8 +22,21 @@ const formatJSON = ({ documents, ...outcome }: Outcome): string =>
 
 const OutcomeCard = ({ outcome }: Props) => (
   <Card>
-    <CardHeader>
-      <CardTitle>{outcome.outcome_title}</CardTitle>
+    <CardHeader className="space-y-0 flex flex-row items-end justify-between">
+      <CardTitle className="text-xl">{outcome.outcome_title}</CardTitle>
+      <div className="flex flex-row space-x-2">
+        <a
+          className="flex flex-row items-center justify-center space-x-2 rounded-full border px-2.5 py-0.5 text-xs border-transparent bg-slate-200 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 hover:bg-slate-300"
+          href={outcome.outcome_url}
+          target="_blank"
+        >
+          Last updated:&nbsp;
+          <time dateTime={outcome.last_updated}>
+            {dayjs(outcome.last_updated).format("D MMMM YYYY")}
+          </time>
+          <ExternalLinkIcon className="size-3" />
+        </a>
+      </div>
     </CardHeader>
     <CardContent>
       <pre>{formatJSON(outcome)}</pre>
