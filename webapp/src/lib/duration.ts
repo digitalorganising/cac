fd36f105ace2c8cd4c098bc91f4d7791969ea1cc
Jamie Parkinson <jamie.parkinson@prospect.org.uk>
2025-06-19 10:22:48 +0100

    Shiny plurals

--- webapp/src/lib/duration.ts
+++ webapp/src/lib/duration.ts
@@ -1,6 +1,10 @@
 import dayjs from "dayjs";
 import { OutcomeKeyDates } from "@/lib/types";
 
+function pluralize(singular: string, quantity: number): string {
+  return quantity === 1 ? singular : `${singular}s`;
+}
+
 export function formatDuration({
   outcomeConcluded,
   applicationReceived,
@@ -10,21 +14,21 @@ export function formatDuration({
 
   const nDays = end.diff(start, "days");
   if (nDays <= 31) {
-    return `${nDays} days`;
+    return `${nDays} ${pluralize("day", nDays)}`;
   }
 
   const nWeeks = Math.floor(nDays / 7);
   if (nWeeks <= 8) {
     const remainder = nDays % 7;
-    return `${nWeeks} weeks, ${remainder} days`;
+    return `${nWeeks} ${pluralize("week", nWeeks)}, ${remainder} ${pluralize("day", remainder)}`;
   }
 
   const nMonths = end.diff(start, "months");
   const nYears = Math.floor(nMonths / 12);
   const remainder = end.diff(start.add(nMonths, "months"), "days");
   if (nYears < 1) {
-    return `${nMonths} months, ${remainder} days`;
+    return `${nMonths} ${pluralize("month", nMonths)}, ${remainder} ${pluralize("day", remainder)}`;
   } else {
-    return `${nYears} years, ${nMonths % 12} months, ${remainder} days`;
+    return `${nYears} ${pluralize("year", nYears)}, ${nMonths % 12} ${pluralize("month", nMonths % 12)}, ${remainder} ${pluralize("day", remainder)}`;
   }
 }
