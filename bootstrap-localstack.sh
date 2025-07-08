31ce28932b3e80e08b59f704683b0b92de3f5244
jamie <jamie.y.parkinson@gmail.com>
2025-07-08 21:28:02 +0100

    CI madness

---
+++ bootstrap-localstack.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+uvx --from awscli-local awslocal sqs create-queue --queue-name test-queue
--- terraform/bootstrap-local.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export AWS_ENDPOINT_URL=http://localhost:4566
-
-TFLOCAL=(uvx --from terraform-local tflocal)
-
-"${TFLOCAL[@]}" init -reconfigure
-"${TFLOCAL[@]}" apply -auto-approve -target=aws_sqs_queue.scraped_items
