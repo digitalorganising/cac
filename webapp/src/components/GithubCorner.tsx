ddfd9937a22aec670331052023e69d417811e8f7
jamie <jamie.y.parkinson@gmail.com>
2025-10-24 19:20:38 +0100

    Better hover state

--- webapp/src/components/GithubCorner.tsx
+++ webapp/src/components/GithubCorner.tsx
@@ -8,13 +8,10 @@ export default function GithubCorner({ href }: { href: string }) {
       aria-label="View source on GitHub"
     >
       <svg
+        className="text-white fill-slate-300 group-hover:fill-slate-400"
         width="80"
         height="80"
         viewBox="0 0 250 250"
-        style={{
-          fill: "var(--color-slate-300)",
-          color: "#fff",
-        }}
         aria-hidden="true"
       >
         <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
